b3c114a757683f18bfd5cbab9e729c649aecd25b
Jazielle <danieljazieldiazgorena@gmail.com>
2025-04-24 23:02:54 -0400

    Actualización API + estilos

---
+++ app/src/main/java/com/example/catalogoanimales/MenuActivity.java
@@ -0,0 +1,24 @@
+package com.example.catalogoanimales;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MenuActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+        Button btnListado = findViewById(R.id.btnListado);
+        btnListado.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MenuActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+} 
